51a51469eb5c233d421e101dca7fc3e40608a09d
andrewakagawa <andrewakagawa@gmail.com>
2017-06-23 22:30:14 -0700

    front end content updates

--- js/main.js
+++ js/main.js
@@ -4,15 +4,6 @@ function main() {
 	(function () {
 		'use strict';
 	
-		// shrinking navbar
-		$(window).scroll(function() {
-			if ($(document).scrollTop() > 50) {
-				$('nav').addClass('shrink');
-			} else {
-				$('nav').removeClass('shrink');
-			}
-		});
-		
 		// Preloader */
   		$(window).load(function() {
 	
